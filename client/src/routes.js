f38fe8797a97e44a6958c3935deb86d23af7b176
Patrick Ofilada <pcofilada@gmail.com>
2020-03-22 16:10:40 +0800

    Add MainLayout

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,11 +2,23 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main';
+import MainLayout from './layouts/MainLayout';
+
+const AppRoute = ({ component: Component, layout: Layout, ...rest }) => (
+  <Route
+    render={props => (
+      <Layout>
+        <Component {...props} />
+      </Layout>
+    )}
+    {...rest}
+  />
+);
 
 export default () => (
   <Router>
     <Switch>
-      <Route path="/" component={Main} />
+      <AppRoute path="/" component={Main} layout={MainLayout} />
     </Switch>
   </Router>
 );
