d97e6a0748f3d83e7a08989c7f37fc81b922004b
Patrick Ofilada <pcofilada@gmail.com>
2020-03-25 09:11:42 +0800

    Add ask for help button

--- client/src/components/HeaderContent.js
+++ client/src/components/HeaderContent.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
+import { Typography, Button } from '@material-ui/core';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
+
+const style = {
+  display: 'flex',
+  width: '100%',
+  justifyContent: 'space-between',
+  alignItems: 'center'
+};
 
 const HeaderContent = () => (
-  <Typography noWrap color={'textSecondary'}>
-    Tulong.PH
-  </Typography>
+  <div style={style}>
+    <Typography noWrap color={'textSecondary'}>
+      Tulong.PH
+    </Typography>
+    <Button
+      variant="contained"
+      color="primary"
+      startIcon={<LocalHospitalIcon />}
+    >
+      Ask For Help!
+    </Button>
+  </div>
 );
 
 export default HeaderContent;
