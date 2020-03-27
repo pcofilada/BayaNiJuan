d426d829ec3b9f97a39b9782449428f7a137afa1
Patrick Ofilada <pcofilada@gmail.com>
2020-03-28 01:48:47 +0800

    Install jwt

---
+++ api/app/services/json_web_token.rb
@@ -0,0 +1,17 @@
+class JsonWebToken
+  SECRET_KEY_BASE = Rails.application.credentials.dig(:secret_key_base)
+
+  def self.encode(id)
+    payload = { user_id: id }
+
+    JWT.encode(payload, SECRET_KEY_BASE)
+  end
+
+  def self.decode(token)
+    token = JWT.decode(token, SECRET_KEY_BASE)[0]
+
+    HashWithIndifferentAccess.new(token)
+  rescue StandardError
+    { status: 403, message: 'Token mismatch' }
+  end
+end
